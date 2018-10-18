bca3c541e538982fdd6ad86008c6b57fa12ce2a2
Christopher <christopher.00.ramos@gmail.com>
2018-10-18 13:29:26 -0700

    Navigation Fixed
    - When a user is logged in they will no longer see "Sign Up/Log In"

--- js/_style-picker.js
+++ js/_style-picker.js
@@ -33,11 +33,14 @@ if (window.netlifyIdentity) {
 		}
 	});
 }
-if (netlifyIdentity.currentUser() == null ) {
-	var generateHere = document.getElementById("nav-actions");
-	generateHere.innerHTML = '<a onclick="netlifyIdentity.open();"><span>Sign Up</span></a><a onclick="netlifyIdentity.open();"><span>Log In</span></a>';
-}
-else {
-	var generateHere = document.getElementById("nav-actions");
-	generateHere.innerHTML = '<a onclick="netlifyIdentity.open();"><span>Logged In</span></a>';
+document.getElementById("nav-actions").addEventListener("load", signedIn);
+	function signedIn() {
+	if (netlifyIdentity.currentUser() == null ) {
+		var generateHere = document.getElementById("nav-actions");
+		generateHere.innerHTML = '<a onclick="netlifyIdentity.open();"><span>Sign Up</span></a><a onclick="netlifyIdentity.open();"><span>Log In</span></a>';
+	}
+	else {
+		var generateHere = document.getElementById("nav-actions");
+		generateHere.innerHTML = '<a onclick="netlifyIdentity.open();"><span>Logged In</span></a>';
+	}
 }
