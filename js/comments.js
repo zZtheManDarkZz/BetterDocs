1a95896b59eb9229cb634e6b0f76fd1ebc1fcb04
Christopher <christopher.00.ramos@gmail.com>
2018-10-18 18:40:33 -0700

    Fixed comments

--- js/comments.js
+++ js/comments.js
@@ -1,5 +1,3 @@
-document.addEventListener("DOMContentLoaded", function(DoGithubComments) { 
-
 var CurrentPage = 0;
 
 function ParseLinkHeader(link)
@@ -88,4 +86,3 @@ function DoGithubComments(comment_id, page_id)
         });
     });
 }
-});
